5c6aa6f1c8b65f691a28bbcca5c2b7ffbcf0d410
VazAnton <vazev.anton@yandex.ru>
2024-04-22 22:39:56 +0700

    style: Пропустил лишний импорт

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.mappers.EntityMapper;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import javax.validation.Valid;
