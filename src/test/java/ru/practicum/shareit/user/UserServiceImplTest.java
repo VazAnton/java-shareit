ec066f0d408c3c7879d75255944b747c747bab16
VazAnton <vazev.anton@yandex.ru>
2024-05-10 19:44:06 +0700

    style: Удалил лишние импорты

--- src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -16,7 +16,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @Transactional
@@ -33,6 +32,7 @@ public class UserServiceImplTest {
     User userDtoToUser;
     User user1;
     User user2;
+
     @BeforeEach
     public void setup() {
         userDto = new UserDto(
